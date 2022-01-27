1bef144dd4aa9c4e8ba628a5eaba72401b101f9f
Lauren Murphy <Lmurphy@gmx.com>
2022-01-27 15:11:59 -0800

    Finished saveToLocalStorage todo

--- js/app.js
+++ js/app.js
@@ -13,10 +13,14 @@ Cart.prototype.addItem = function(product, quantity) {
 };
 
 Cart.prototype.saveToLocalStorage = function() {
-  // TODO: Fill in this instance method to save the contents of the cart to localStorage
+  // DONE: Fill in this instance method to save the contents of the cart to localStorage
+  let stringifiedCart = JSON.stringify(cart);
+  localStorage.setItem('savedCart',stringifiedCart);
+  //console.log(localStorage.getItem('savedCart'));
 };
 
 Cart.prototype.removeItem = function(item) {
+  
   // TODO: Fill in this instance method to remove one item from the cart.
   // Note: You will have to decide what kind of parameter to pass in here!
 };
